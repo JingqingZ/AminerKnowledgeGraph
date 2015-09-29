c9f2a560cf57383eed4544cd37e28686e8315cb0
JingqingZ <zhangjqsmiling@gmail.com>
2015-09-29 14:38:27 +0800

    more data

--- views/generate_view.py
+++ views/generate_view.py
@@ -20,30 +20,44 @@ for line in timelinefile:
         dictionary[keyword] = {}
 timelinefile.close()
 
-linkdifffile = open("../results/link_diff.list", "r")
+linkdifffile = open("../results/link_diff_trend_sim.list", "r")
 linklist = []
 for line in linkdifffile:
-    line = line.split(" ")
+    line = line.replace("\n", "").split(" ")
     linklist.append(line)
 linkdifffile.close()
 
 htmlfile = open("index.html", "w")
 htmlfile.write('<html><head><script src="static/raphael-min.js"></script><script src="static/jquery-1.8.2.min.js"></script><script src="static/morris-0.4.1.min.js"></script><meta charset=utf-8 /><title>Knowledge Trend Check</title></head><body>')
-htmlfile.write('<div id="results" style="display: none">')
-for i in range(0, len(linklist)):
-    htmlfile.write('<p id="link' + repr(i) + '-results">0</p>')
-htmlfile.write('</div>')
+htmlfile.write('<div id="results" style="display: none">\n')
 valid = 0
 for i in range(0, len(linklist)):
     key0 = stem(linklist[i][0])
     key1 = stem(linklist[i][1])
     if key0 not in dictionary or key1 not in dictionary:
         continue
-    valid += 1 
+    valid += 1
+    if valid > 30:
+        break
+    htmlfile.write('<p id="link' + repr(i) + '-results">0</p>\n')
+htmlfile.write('</div>\n')
+htmlfile.write('<p>说明1：请在你认为正确的衍生关系前打勾</p>')
+htmlfile.write('<p>说明2：frequency代表发表的论文中有该先后关系的作者数，仅供参考</p>')
+htmlfile.write('<p>说明3：趋势图代表该关键词每年在多少篇论文中有出现，仅供参考</p>')
+htmlfile.write('<p>说明4：trend similarity代表两个关键词趋势图的相似性，1.0为完全相同</p>')
+htmlfile.write('<p  style="margin-top: 100px">----------------------------------------------------------------------------------------------------------------------------------------------------------------</p>')
+valid = 0
+for i in range(0, len(linklist)):
+    key0 = stem(linklist[i][0])
+    key1 = stem(linklist[i][1])
+    if key0 not in dictionary or key1 not in dictionary:
+        continue
+    valid += 1
     if valid > 30:
         break
     htmlfile.write('<input type="checkbox" id="link' + repr(i) + '-checkbox1"> ' + linklist[i][0].replace("_", " ") + ' --> ' + linklist[i][1].replace("_", " ") + ' (frequency ' + linklist[i][2] + ')<br>')
     htmlfile.write('<input type="checkbox" id="link' + repr(i) + '-checkbox2"> ' + linklist[i][1].replace("_", " ") + ' --> ' + linklist[i][0].replace("_", " ") + ' (frequency ' + linklist[i][3] + ')<br>')
+    htmlfile.write('<p> trend similarity ' + linklist[i][5] + ' (注：1.0为完全相同) </p>')
     htmlfile.write('<div id="link' + repr(i) + '" style="height: 200px; width: 50%"></div>')
     htmlfile.write('<script id="jsbin-javascript">')
     htmlfile.write('$("#link' + repr(i) + '-checkbox1").click(function() {')
