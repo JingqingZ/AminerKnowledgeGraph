7226b9c7d456818848e095c3f30ae8870818077e
JingqingZ <zhangjqsmiling@gmail.com>
2015-10-04 14:07:05 +0800

    update html generator

--- views/generate_view.py
+++ views/generate_view.py
@@ -1,10 +1,15 @@
 # generate view
 
 from stemming.porter2 import stem
+import sys
+
+query = sys.argv[1].replace(" ", "_")
+sim_threshold = 0.85
+graph_num = 30
 
 minyear = 1980
 maxyear = 2014
-timelinefile = open("../results/pub_high_performances_computing.dist", "r")
+timelinefile = open("../results/pub_" + query + ".dist", "r")
 dictionary = {}
 keyword = ""
 for line in timelinefile:
@@ -20,24 +25,45 @@ for line in timelinefile:
         dictionary[keyword] = {}
 timelinefile.close()
 
-linkdifffile = open("../results/trend_sim_high_performances_computing.list", "r")
+linkdifffile = open("../results/trend_sim_" + query + ".list", "r")
 linklist = []
 for line in linkdifffile:
     line = line.replace("\n", "").split(" ")
     linklist.append(line)
 linkdifffile.close()
 
-htmlfile = open("index.html", "w")
-htmlfile.write('<html><head><script src="static/raphael-min.js"></script><script src="static/jquery-1.8.2.min.js"></script><script src="static/morris-0.4.1.min.js"></script><meta charset=utf-8 /><title>Knowledge Trend Check</title></head><body>')
+htmlfile = open(query + ".html", "w")
+
+htmlfile.write('<html><head>')
+jsfile = open("static/jquery-1.8.2.min.js", 'r')
+htmlfile.write('<script>')
+for line in jsfile:
+    htmlfile.write(line)
+htmlfile.write('</script>')
+jsfile.close()
+jsfile = open("static/raphael-min.js", 'r')
+htmlfile.write('<script>')
+for line in jsfile:
+    htmlfile.write(line)
+htmlfile.write('</script>')
+jsfile.close()
+jsfile = open("static/morris-0.4.1.min.js", 'r')
+htmlfile.write('<script>')
+for line in jsfile:
+    htmlfile.write(line)
+htmlfile.write('</script>')
+jsfile.close()
+htmlfile.write('<meta charset=utf-8 /><title>Knowledge Trend Check</title></head><body>')
+
 htmlfile.write('<div id="results" style="display: none">\n')
 valid = 0
 for i in range(0, len(linklist)):
     key0 = stem(linklist[i][0])
     key1 = stem(linklist[i][1])
-    if key0 not in dictionary or key1 not in dictionary or float(linklist[i][5]) > 0.5:
+    if key0 not in dictionary or key1 not in dictionary or float(linklist[i][5]) > sim_threshold:
         continue
     valid += 1
-    if valid > 30:
+    if valid > graph_num:
         break
     htmlfile.write('<p id="link' + repr(i) + '-results">0</p>\n')
 htmlfile.write('</div>\n')
@@ -50,10 +76,10 @@ valid = 0
 for i in range(0, len(linklist)):
     key0 = stem(linklist[i][0])
     key1 = stem(linklist[i][1])
-    if key0 not in dictionary or key1 not in dictionary or float(linklist[i][5]) > 0.5:
+    if key0 not in dictionary or key1 not in dictionary or float(linklist[i][5]) > sim_threshold:
         continue
     valid += 1
-    if valid > 30:
+    if valid > graph_num:
         break
     htmlfile.write('<input type="checkbox" id="link' + repr(i) + '-checkbox1"> ' + linklist[i][0].replace("_", " ") + ' --> ' + linklist[i][1].replace("_", " ") + ' (frequency ' + linklist[i][2] + ')<br>')
     htmlfile.write('<input type="checkbox" id="link' + repr(i) + '-checkbox2"> ' + linklist[i][1].replace("_", " ") + ' --> ' + linklist[i][0].replace("_", " ") + ' (frequency ' + linklist[i][3] + ')<br>')
@@ -61,24 +87,24 @@ for i in range(0, len(linklist)):
     htmlfile.write('<div id="link' + repr(i) + '" style="height: 200px; width: 50%"></div>')
     htmlfile.write('<script id="jsbin-javascript">')
     htmlfile.write('$("#link' + repr(i) + '-checkbox1").click(function() {')
-    htmlfile.write('    if(document.getElementById("link' + repr(i) + '-checkbox1").checked == true) {')
-    htmlfile.write('        document.getElementById("link' + repr(i) + '-checkbox2").checked = false;')
-    htmlfile.write('        $("#link' + repr(i) + '-results").html("1");')
-    htmlfile.write('    } else {')
-    htmlfile.write('        $("#link' + repr(i) + '-results").html("0");')
-    htmlfile.write('    }')
+    htmlfile.write('if(document.getElementById("link' + repr(i) + '-checkbox1").checked == true) {')
+    htmlfile.write('document.getElementById("link' + repr(i) + '-checkbox2").checked = false;')
+    htmlfile.write('$("#link' + repr(i) + '-results").html("1");')
+    htmlfile.write('} else {')
+    htmlfile.write('$("#link' + repr(i) + '-results").html("0");')
+    htmlfile.write('}')
     htmlfile.write('});')
     htmlfile.write('$("#link' + repr(i) + '-checkbox2").click(function() {')
-    htmlfile.write('    if(document.getElementById("link' + repr(i) + '-checkbox2").checked == true) {')
-    htmlfile.write('        document.getElementById("link' + repr(i) + '-checkbox1").checked = false;')
-    htmlfile.write('        $("#link' + repr(i) + '-results").html("-1");')
-    htmlfile.write('    } else {')
-    htmlfile.write('        $("#link' + repr(i) + '-results").html("0");')
-    htmlfile.write('    }')
+    htmlfile.write('if(document.getElementById("link' + repr(i) + '-checkbox2").checked == true) {')
+    htmlfile.write('document.getElementById("link' + repr(i) + '-checkbox1").checked = false;')
+    htmlfile.write('$("#link' + repr(i) + '-results").html("-1");')
+    htmlfile.write('} else {')
+    htmlfile.write('$("#link' + repr(i) + '-results").html("0");')
+    htmlfile.write('}')
     htmlfile.write('});')
     htmlfile.write('Morris.Line({')
-    htmlfile.write('  element: "link' + repr(i) + '",')
-    htmlfile.write('  data: [')
+    htmlfile.write('element: "link' + repr(i) + '",')
+    htmlfile.write('data: [')
     for y in range(minyear, maxyear + 1):
         a = 0
         b = 0
