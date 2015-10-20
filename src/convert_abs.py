22957069a33509d346ad0495a270e92d5459c364
2012011320 <ltrthu@163.com>
2015-10-20 10:14:07 +0800

    update abstract extraction and preprocessing

--- src/convert_abs.py
+++ src/convert_abs.py
@@ -1,4 +1,7 @@
 from stemming.porter2 import stem
+import ast
+import re
+import string
 import sys
 
 class ConvertAbstract(object):
@@ -8,8 +11,27 @@ class ConvertAbstract(object):
         self.query = q.replace(' ', '_')
         self.keyword_dict = dict()
 
+    def parse_publication_abstract(self):
+        publication_file = '../data/pub_' + self.query + '.data'
+        output_file = '../results/pub_' + self.query + '.abs'
+        
+        infile = open(publication_file, 'r')
+        output = open(output_file, 'w')
+        cur = 0
+        for line in infile:
+            if cur % 1000 == 0:
+                print (cur)
+            publication = ast.literal_eval(line)
+            abstract = publication['abstract']
+            if len(abstract) < 5:
+                continue
+            output.write(abstract + '\n')
+            cur += 1
+        output.close()
+
     def load_keywords(self):
         content = open('../results/pub_' + self.query + '.keywords').readlines()
+        print ('loading keywords')
         for i in content:
             key = i.split('\t')[0]
             words = key.split('_')
@@ -18,28 +40,34 @@ class ConvertAbstract(object):
                     self.keyword_dict[ words[0] ].add(stem(words[1]))
                 else:
                     self.keyword_dict[ words[0] ] = { stem(words[1]) }
+        print ('load keywords complete')
 
     def parse_abstract(self):
         content = open('../results/pub_' + self.query + '.abs').readlines()
-        output = open('../results/output', 'w')
+        output = open('../results/pub_' + self.query + '.w2v', 'w')
         cnt = 0
         for i in content:
+            line = re.sub(r'\n|[{}]+'.format(string.punctuation), ' ', i).lower()
             cnt = cnt + 1
             if cnt % 1000 == 0:
                 print (cnt)
-            li = i.split(' ')
-            for j in range(0, len(li)):
-                if li[j] in self.keyword_dict:
-                    if (j+1) < len(li) and stem(li[j+1]) in self.keyword_dict[ li[j] ]:
-                        output.write(li[j] + '_')
-                    else:
-                        output.write(li[j] + ' ')
+            if cnt == 10000:
+                break
+            li = line.split(' ')
+            j = 0
+            while j < len(li):
+                if (j+1) < len(li) and li[j] in self.keyword_dict and stem(li[j+1]) in self.keyword_dict[ li[j] ]:
+                    output.write(li[j] + '_' + li[j+1] + ' ')
+                    j += 2
                 else:
                     output.write(li[j] + ' ')
+                    j += 1
+            output.write('\n')
         output.close()
 
 def main():
     ca = ConvertAbstract(sys.argv[1])
+    #ca.parse_publication_abstract()
     ca.load_keywords()
     ca.parse_abstract()
 
