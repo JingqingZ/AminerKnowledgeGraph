a1cd37f036ec4eee8d187a213a5582b2fd2277fb
JingqingZ <zhangjqsmiling@gmail.com>
2015-11-17 15:27:45 +0800

    update

--- social_tie/src/train.sh
+++ social_tie/src/train.sh
@@ -9,6 +9,6 @@ fi
 ./OpenCRF_Main -inf -niter 30 -nbipter 30 -srcmodel ./train_model.txt -method gradient -gradientstep 0.02 -hasvalue -trainfile ../results/$1/train.txt -testfile ../results/$1/unlabel.txt
 cp ./pred.txt ../results/$1/pred.txt
 # python3.4 generate_predict_link.py
-cp ../results/$1/results.txt ../../views/pattern_analysis/results/$1/results.txt
-cp ../results/$1/results_label.txt ../../views/pattern_analysis/results/$1/results_label.txt
-cp ../results/$1/results_pred.txt ../../views/pattern_analysis/results/$1/results_pred.txt
+# cp ../results/$1/results.txt ../../views/pattern_analysis/results/$1/results.txt
+# cp ../results/$1/results_label.txt ../../views/pattern_analysis/results/$1/results_label.txt
+# cp ../results/$1/results_pred.txt ../../views/pattern_analysis/results/$1/results_pred.txt
